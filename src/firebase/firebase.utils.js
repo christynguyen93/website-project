085a14009f0c320d55f1f8c5fe9831ebd4492220
chaunguyen1093 <48667936+chaunguyen1093@users.noreply.github.com>
2021-12-27 21:02:46 -0600

    copying stuff over

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const config = {
+    apiKey: "AIzaSyDJ2W2fKuumpI6-jHA673BdG3HiCe8Uvlc",
+    authDomain: "crwn-db-a1322.firebaseapp.com",
+    projectId: "crwn-db-a1322",
+    storageBucket: "crwn-db-a1322.appspot.com",
+    messagingSenderId: "785606838580",
+    appId: "1:785606838580:web:7dbaf7db3f9f6d6ce03d61"
+  };
+
+  firebase.initializeApp(config);
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: 'select_account'});
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
