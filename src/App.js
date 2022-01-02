02d2fc78f8f6316fc71d64a09d3b6b57980cf9d9
chaunguyen1093 <48667936+chaunguyen1093@users.noreply.github.com>
2022-01-02 16:28:58 -0600

    adding redux, using currentUser in App.js

--- src/App.js
+++ src/App.js
@@ -8,33 +8,28 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument} from './firebase/firebase.utils';
+import {connect} from 'react-redux';
+import {setCurrentUser} from './redux/user/user.actions';
 class App extends React.Component{
-  constructor(){
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromauth = null;
 
   componentDidMount(){
+    const {setCurrentUser} = this.props;
     this.unsubscribeFromauth = auth.onAuthStateChanged( async userAuth => {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
-          });
+            })
           console.log(this.state);
         });
-      }
+      } 
       else {
-        this.setState({currentUser: userAuth});
+        setCurrentUser(userAuth);
       }
     })
   }
@@ -46,7 +41,7 @@ componentWillUnmount(){
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header/>
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -57,5 +52,7 @@ componentWillUnmount(){
   }
 } 
 
-
-export default App;
+const mapDispatchToProps = dispatch =>({
+setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+export default connect(null,mapDispatchToProps)(App);
